f3291ec377479bbc3b7d5e42ec39ca803137f0f0
tesag <>
2023-02-23 06:39:55 +0000

    File Práctica 1.2 Punteros_3 created by HW helper

---
+++ Parcial1/Práctica 1.2 Punteros_3.cpp
@@ -0,0 +1,12 @@
+print("¡Hola mundo!") #include <iostream>
+
+using namespace std;
+
+int main() {
+ cout << "¡Hola mundo!" << endl;
+ return 0;
+} public class HolaMundo {
+ public static void main(String[] args) {
+ System.out.println("¡Hola mundo!");
+ }
+}
